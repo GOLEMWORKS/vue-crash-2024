4c62dc2fd45c6e8f19aad99968381227849f560b
GOLEMWORKS <korotkih.vladimir.isr21.1@gmail.com>
2024-09-25 17:38:14 +0200

    29:51  https://www.youtube.com/watch?v=VeNfHj6MhgA

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
+<script>
+    export default{
+      data() {
+        return {
+          name: 'Иван Иванов',
+          status: true,
+        }
+      }
+    }
+</script>
+
 <template>
-Hello world!
+  <h1>Hello {{name}}!</h1>
+  <p v-if="status">User is active</p>
+  <p v-else="status">User is innactive</p>
 </template>
